219da759759ff907d911fbf43bf32f6d23b0b0b8
chenjianbin <546391242@qq.com>
2016-06-22 16:09:09 +0800

    update

--- release.py
+++ release.py
@@ -14,6 +14,9 @@ DOMAIN = CONFIG.get('site_static', SITE)
 INS = common.hosts.Hosts(SITE)
 HOSTS = INS.get()
 
+#class Release(object):
+#	def __init__(self, domain):
+#		self.domain = domain
 def release():
 	for h in HOSTS:
 		ins = sync.git.Git(h, D, SITE)
@@ -36,50 +39,6 @@ def release():
 		print('no file need to flush!')
 	print('%s' % '#'*len(flush_cdn_message))
 		
-
-#def get_m_file():
-#	configfile = os.path.split(os.path.realpath(__file__))[0] + '/' + 'config/aliyun.conf'
-#	config = configparser.ConfigParser()
-#	config.read(configfile)
-#	filetypes = config.get('cdn', 'filetypes')
-#	ins = sync.git.Git(HOSTS[0], D, SITE)
-#	stdout, stderr = ins.get_diff_files()
-#	files = []
-#	for i in stdout:
-#		if re.match('M.*?\.%s' % filetypes, i):
-#			files.append(i[1:].strip())
-#	return files	
-	
-
-#def flush_cdn():
-#	config = configparser.ConfigParser()
-#	config.read(CONFIGFILE)
-#	domain = config.get('site_static', SITE)
-#	args_dict = {}
-#	args_dict['Action'] = 'RefreshObjectCaches'
-#	args_dict['ObjectType'] = 'File'
-#	ins = sync.git.Git(HOSTS[0], D, SITE)
-#	files = ins.get_m_files()
-#	if files:
-#		for f in files:
-#			args_dict['ObjectPath'] = DOMAIN + '/' + f
-#			print('flush http://%s' % args_dict['ObjectPath'])
-#			ins = format_url.Format(args_dict)
-#			url = ins.compose_url()
-#			res = req_flush_api(url)
-#			print(res)
-#			
-#	else:
-#		print('no file need to flush!')
-
-#def req_flush_api(url):
-#	try:
-#		with urllib.request.urlopen(url) as res:
-#			res = res.read()
-#		res = res.decode('utf-8')
-#		return res
-#	except urllib.error.HTTPError as res:
-#		return res
 
 
 if __name__ == '__main__':
